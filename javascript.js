46cf5de76c5f967e3f7595b07f0bd22d9e9598cb
elcharbs <60406297+elcharbs@users.noreply.github.com>
2023-12-06 14:36:18 -0500

    Solve the whole project

--- javascript.js
+++ javascript.js
@@ -1,22 +1,49 @@
-console.log(`Hello world!`);
-
 const switchBtn = document.querySelector('input[type="number"]');
 const gridContainer = document.querySelector('.container');
+const gridSquare = document.querySelector('.gridSquare');
 
-function addGridSquare () {
-    let newSquare = document.createElement('div');
-    newSquare.classList.add('gridSquare')
-    gridContainer.appendChild(newSquare);
+// Initial script to add squares
+function drawGrid (squareCount) {
+    console.log("initialGridLoad started");
+    // Clear container
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+    addGridSquare(gridContainer, squareCount);
+    let topRowSquares = document.querySelectorAll('.gridSquare');
+    for (let i = 0; i < topRowSquares.length; i++) {
+        addGridSquare(topRowSquares[i], squareCount);
+    }
+    updateHoverListeners();
 }
 
-function removeGridSquare () {
-    grid
+function addGridSquare (container, count) {
+    for (let i = 0; i < count; i++) {
+        console.log("addGridSquare initiated");
+        let newSquare = document.createElement('div');
+        newSquare.classList.add('gridSquare')
+        container.appendChild(newSquare);
+    }
 }
 
-console.log(switchBtn);
 switchBtn.addEventListener('change', () => {
     let desiredSquareCount = document.querySelector('#squareCount').value;
     console.log(`Desired square count changed to ${desiredSquareCount}`);
-    addGridSquare();
+    drawGrid(desiredSquareCount);
 }
 )
+
+function updateHoverListeners () {
+    gridSquares =  document.querySelectorAll('.gridSquare');
+    console.log(gridSquares);
+    gridSquares.forEach((square) => {
+        console.log(square);
+        square.addEventListener('mouseover', () => {
+            console.log("OOOO A MOUSE!");
+            square.classList.add('blackSquare');
+        })
+    })     
+};
+
+// Loads initial 4x4 grid
+drawGrid(4)
